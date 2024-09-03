f32e13654bc31457d00b642e17e7583535afe4aa
RomeoZach <irfan.zak7651@gmail.com>
2024-09-03 21:05:54 +0700

    acumalaka

---
+++ ContohVariableNoAbsen.java
@@ -0,0 +1,15 @@
+public class ContohVariableNoAbsen {
+    public static void main(String args[]){
+    String SalahSatuHobySayaAdalah = "Olahraga";
+    boolean isPandai = true;
+    char jeniskelamin = 'L';
+    byte _umurSayaSekarang = 20;
+    double $ipk = 3.24, tinggi = 1.78;
+    System.out.println(SalahSatuHobySayaAdalah);
+    System.out.println("Apakah pandai7" + isPandai);
+    System.out.println("Jenis Kelamin:" + jeniskelamin);
+    System.out.println("Umurku saat ini:" + _umurSayaSekarang);
+    System.out.println(String.format("saya beripk %5, dengan tinggi badan %5", $ipk, tinggi));
+    }
+
+    }
